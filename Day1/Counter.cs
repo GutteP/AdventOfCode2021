19ab19f9c6872f90e62a3d903b9fe128d48c1d75
Gustav Pettersson <Gustav.Pettersson@knowit.se>
2021-12-01 10:24:48 +0100

    Add project files.

---
+++ Day1/Counter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1
+{
+    public static class Counter
+    {
+        public static int TimesIncreased(IEnumerable<int> depths)
+        {
+            int lastValue = int.MaxValue;
+            int timesIncreased = 0;
+            foreach (var depth in depths)
+            {
+                if (depth > lastValue) timesIncreased++;
+                lastValue = depth;
+            }
+            return timesIncreased;
+        }
+    }
+}
