e6e46928b27e27d2b1f2521be9f487cb924c1687
Gustav Pettersson <Gustav.Pettersson@knowit.se>
2021-12-03 08:57:47 +0100

    Dag 3 och prepp för dag 4

---
+++ Day 4/Program.cs
@@ -0,0 +1,6 @@
+﻿using Library.Helpers;
+
+Console.WriteLine("Day 4!");
+Console.WriteLine("\nPart 1\n");
+
+var input = InputHelper.ReadTextFile("input.txt");
--- Day3/Program.cs
+++ Day3/Program.cs
@@ -1,5 +1,67 @@
 ﻿using Library.Helpers;
+using System.Linq;
 
 Console.WriteLine("Day 3!");
+Console.WriteLine("\nPart 1\n");
 
-IEnumerable<string> input = InputHelper.ReadTextFile("input.txt");
+string binaryGammaRate = "";
+string binaryEpsilonRate = "";
+
+for (int i = 0; i < 12; i++)
+{
+    int numberOf1 = 0;
+    int numberOf0 = 0;
+    foreach (string row in InputHelper.ReadTextFile("input.txt"))
+    {
+        if (row[i] == '1') numberOf1++;
+        else numberOf0++;
+    }
+
+    if (numberOf1 > numberOf0)
+    {
+        binaryGammaRate += '1';
+        binaryEpsilonRate += '0';
+    }
+    else
+    {
+        binaryGammaRate += '0';
+        binaryEpsilonRate += '1';
+    }
+}
+
+int gammaRate = Convert.ToInt32(binaryGammaRate, 2);
+int epsilonRate = Convert.ToInt32(binaryEpsilonRate, 2);
+
+Console.WriteLine($"What is the power consumption of the submarine? GammaRate: {gammaRate}, epsilonRate: {epsilonRate}, multi: {gammaRate * epsilonRate}");
+
+
+Console.WriteLine("\nPart 2\n");
+
+var mostCommon = InputHelper.ReadComplateTextFile("input.txt").ToList();
+var leastCommon = InputHelper.ReadComplateTextFile("input.txt").ToList();
+
+for (int i = 0; i < 12; i++)
+{
+    if(mostCommon.Count() > 1)
+    {
+        var oneC = mostCommon.Where(x => x[i] == '1').ToList();
+        var zeroC = mostCommon.Where(x => x[i] == '0').ToList();
+
+        if (oneC.Count() >= zeroC.Count()) mostCommon = oneC;
+        else mostCommon = zeroC;
+    }
+    
+    if(leastCommon.Count() > 1)
+    {
+        var oneL = leastCommon.Where(x => x[i] == '1').ToList();
+        var zeroL = leastCommon.Where(x => x[i] == '0').ToList();
+
+        if (oneL.Count() >= zeroL.Count()) leastCommon = zeroL;
+        else leastCommon = oneL;
+    }   
+}
+
+int generator = Convert.ToInt32(mostCommon.First(), 2);
+int scrubber = Convert.ToInt32(leastCommon.First(), 2);
+
+Console.WriteLine($"What is the life support rating of the submarine? oxygen generator rating: {generator}, CO2 scrubber rating: {scrubber}, multi: {generator * scrubber}");
