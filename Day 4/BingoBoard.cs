cd8261e603112b2e74254d8f423a99dc7b804e52
Gustav Pettersson <gustav.pettersson@knowit.se>
2021-12-04 11:51:11 +0100

    dag 4 del 1

---
+++ Day 4/BingoBoard.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_4
+{
+    public class BingoBoard
+    {
+        private List<List<(int Value, bool Set)>> _board;
+
+        public BingoBoard(IEnumerable<string> input)
+        {
+            _board = new List<List<(int, bool)>>();
+            foreach (var row in input)
+            {
+                string[] values = row.Trim().Split(' ');
+                List<(int, bool)> r = new();
+                foreach (string value in values)
+                {
+                    if (value == "") continue;
+                    r.Add((int.Parse(value), false));
+                }
+                _board.Add(r);
+            }
+        }
+
+        public bool Bingo(int num)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (_board[i][j].Value == num)
+                    {
+                        _board[i][j] = (_board[i][j].Value, true);
+                        if (Check(i, j)) return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private bool Check(int i, int j)
+        {
+            bool bingo = true;
+            for (int k = 0; k < 5; k++)
+            {
+                if (!_board[i][k].Set) bingo = false;
+
+            }
+            if (bingo) return bingo;
+            bingo = true;
+            for (int k = 0; k < 5; k++)
+            {
+                if (!_board[k][j].Set) bingo = false;
+
+            }
+            return bingo;
+        }
+
+        public int Calculate(int num)
+        {
+            int sum = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (!_board[i][j].Set) sum += _board[i][j].Value;
+                }
+            }
+            return sum * num;
+        }
+    }
+}
--- Day 4/Program.cs
+++ Day 4/Program.cs
@@ -1,6 +1,37 @@
-﻿using Library.Helpers;
+﻿using Day_4;
+using Library.Helpers;
 
 Console.WriteLine("Day 4!");
 Console.WriteLine("\nPart 1\n");
 
-var input = InputHelper.ReadTextFile("input.txt");
+List<BingoBoard> _boards = new();
+
+var input = InputHelper.ReadTextFile("input.txt").ToList();
+
+string randomNrs = input[0];
+input.RemoveAt(0);
+
+while(input.Count > 0)
+{
+    if(input[0] == "") input.RemoveAt(0);
+    else
+    {
+        _boards.Add(new BingoBoard(input.Take(5)));
+        input.RemoveRange(0, 5);
+    }
+}
+
+foreach (string num in randomNrs.Split(','))
+{
+    bool done = false;
+    foreach (BingoBoard board in _boards)
+    {
+        if (board.Bingo(int.Parse(num)))
+        {
+            Console.WriteLine($"BINGO! Result: {board.Calculate(int.Parse(num))}");
+            done = true;
+            break;
+        }
+    }
+    if (done) break;
+}
