abbd8286321be3763b1d1c4932d519ab8475c615
Gustav Pettersson <Gustav.Pettersson@knowit.se>
2021-12-09 10:02:05 +0100

    Dag 9

---
+++ Day9/FloorMapper.cs
@@ -0,0 +1,82 @@
+﻿
+namespace Day9
+{
+    public static class FloorMapper
+    {
+        public static bool IsLowPoint(this int[,] floor, (int i, int j) pos)
+        {
+            int depth = floor.GetLength(0)-1;
+            int width = floor.GetLength(1)-1;
+
+            int v = floor[pos.i, pos.j];
+            if(pos.i != 0)
+            {
+                if (floor[pos.i - 1,pos.j] <= v) return false;
+            }
+            if(pos.j != 0)
+            {
+                if (floor[pos.i, pos.j-1] <= v) return false;
+            }
+            if (pos.i != depth)
+            {
+                if (floor[pos.i + 1, pos.j] <= v) return false;
+            }
+            if (pos.j != width)
+            {
+                if (floor[pos.i, pos.j + 1] <= v) return false;
+            }
+            return true;
+        }
+
+        public static int ClaculateRiskLevel(this int[,] floor, List<(int i, int j)> lowPoints)
+        {
+            int riskLevel = 0;
+            foreach (var pos in lowPoints)
+            {
+                riskLevel += (floor[pos.i, pos.j] + 1);
+            }
+
+            return riskLevel;
+        }
+
+        public static int SizeOfBasin(this int[,] floor, (int i, int j) lowPoint)
+        {
+            HashSet<(int, int)> partsOfBasin = new();
+            partsOfBasin.Add(lowPoint);
+            partsOfBasin.UnionWith(floor.IsBasin(lowPoint));
+            return partsOfBasin.Count;
+        }
+
+        private static HashSet<(int, int)> IsBasin(this int[,] floor, (int i, int j) pos, HashSet<(int, int)> discoverd = null)
+        {
+            if (discoverd == null) discoverd = new();
+            int depth = floor.GetLength(0) - 1;
+            int width = floor.GetLength(1) - 1;
+            int v = floor[pos.i, pos.j];
+            HashSet<(int, int)> newParts = new();
+            if (pos.i != 0)
+            {
+                if (floor[pos.i - 1, pos.j] != 9 && !discoverd.Contains((pos.i - 1, pos.j))) newParts.Add((pos.i - 1, pos.j));
+            }
+            if (pos.j != 0)
+            {
+                if (floor[pos.i, pos.j - 1] != 9 && !discoverd.Contains((pos.i, pos.j - 1))) newParts.Add((pos.i, pos.j - 1));
+            }
+            if (pos.i != depth)
+            {
+                if (floor[pos.i + 1, pos.j] != 9 && !discoverd.Contains((pos.i + 1, pos.j))) newParts.Add((pos.i + 1, pos.j));
+            }
+            if (pos.j != width)
+            {
+                if (floor[pos.i, pos.j + 1] != 9 && !discoverd.Contains((pos.i, pos.j + 1))) newParts.Add((pos.i, pos.j + 1));
+            }
+            var l = newParts.Count;
+            for (int i = 0; i < l; i++)
+            {
+                discoverd.UnionWith(newParts);
+                newParts.UnionWith(floor.IsBasin(newParts.ElementAt(i), discoverd));
+            }
+            return newParts;
+        }
+    }
+}
---
+++ Day9/Program.cs
@@ -0,0 +1,47 @@
+﻿
+using Day9;
+using Library.Helpers;
+
+Console.WriteLine("--- Day 9: Smoke Basin ---");
+Console.WriteLine("\nPart 1");
+
+//int[,] floor = new int[5, 10];
+int[,] floor = new int[100, 100];
+int fI = 0;
+foreach (var row in InputHelper.ReadTextFile("input.txt"))
+{
+    for (int j = 0; j < row.Length; j++)
+    {
+        var c = row[j];
+        floor[fI, j] = int.Parse(String.Concat(c));
+    }
+    fI++;
+}
+
+
+List<(int i, int j)> lowPoints = new();
+for (int i = 0; i < floor.GetLength(0); i++)
+{
+    for (int j = 0; j < floor.GetLength(1); j++)
+    {
+        if (floor.IsLowPoint((i, j))) lowPoints.Add((i, j));
+    }
+}
+
+int riskLevel = floor.ClaculateRiskLevel(lowPoints);
+
+Console.WriteLine($"Find all of the low points on your heightmap. What is the sum of the risk levels of all low points on your heightmap? {riskLevel}");
+
+
+Console.WriteLine("\nPart 2");
+
+List<int> basinSizes = new();
+foreach (var lowPoint in lowPoints)
+{
+    basinSizes.Add(floor.SizeOfBasin(lowPoint));
+}
+
+List<int> biggest3 = basinSizes.OrderByDescending(x => x).Take(3).ToList();
+
+
+Console.WriteLine($"What do you get if you multiply together the sizes of the three largest basins? {biggest3[0] * biggest3[1] * biggest3[2]}");
