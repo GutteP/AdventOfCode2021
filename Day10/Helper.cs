5ce3b597dd5d85db5ec1b372b24bada3a67ea085
Gustav Pettersson <Gustav.Pettersson@knowit.se>
2021-12-10 08:38:57 +0100

    Dag 10, fungerande lösning

---
+++ Day10/Helper.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day10
+{
+
+    public static class Helper
+    {
+        static char o1 = '(';
+        static char o2 = '[';
+        static char o3 = '{';
+        static char o4 = '<';
+        static char c1 = ')';
+        static char c2 = ']';
+        static char c3 = '}';
+        static char c4 = '>';
+
+        public static bool IsOpen(this char c)
+        {
+            return c == o1 || c == o2 || c == o3 || c == o4;
+        }
+
+        public static bool IsClosedBy(this char o, char c)
+        {
+            if(o == o1 && c == c1) return true;
+            if (o == o2 && c == c2) return true;
+            if (o == o3 && c == c3) return true;
+            if (o == o4 && c == c4) return true;
+            return false;
+        }
+
+        public static char ComplementedBy(this char o)
+        {
+            if (o == o1) return c1;
+            if (o == o2) return c2;
+            if (o == o3) return c3;
+            if (o == o4) return c4;
+            if (o == c1) return o1;
+            if (o == c2) return o2;
+            if (o == c3) return o3;
+            if (o == c4) return o4;
+            else throw new Exception();
+        }
+
+        public static int ClaculateSyntaxErrorScore(this Dictionary<char, int> illegalCharacters)
+        {
+            int score = 0;
+            foreach (var c in illegalCharacters)
+            {
+                score += (c.Key.CharErrorPoints() * c.Value);
+            }
+            return score;
+        }
+
+        public static long ClaculateCompletionScore(List<string> compleats)
+        {
+            List<long> scores = new();
+            foreach (string comp in compleats)
+            {
+                long score = 0;
+                foreach (char c in comp)
+                {
+                    score = (5 * score) + c.CharCompletionPoints();
+                }
+                scores.Add(score);
+            }
+            scores = scores.OrderBy(x => x).ToList();
+            return scores[scores.Count / 2];
+        }
+        private static int CharCompletionPoints(this char c)
+        {
+            if (c == c1) return 1;
+            if (c == c2) return 2;
+            if (c == c3) return 3;
+            if (c == c4) return 4;
+            return 0;
+        }
+        private static int CharErrorPoints(this char c)
+        {
+            if (c == c1) return 3;
+            if (c == c2) return 57;
+            if (c == c3) return 1197;
+            if (c == c4) return 25137;
+            return 0;
+        }
+    }
+}
--- Day10/Program.cs
+++ Day10/Program.cs
@@ -1,18 +1,89 @@
-﻿using Library.Helpers;
+﻿using Day10;
+using Library.Helpers;
 
-Console.WriteLine("");
+Console.WriteLine("--- Day 10: Syntax Scoring ---");
 Console.WriteLine("\nPart 1");
 
-foreach (var row in InputHelper.ReadTextFile("test.txt"))
-{
+string o1 = "(";
+string o2 = "[";
+string o3 = "{";
+string o4 = "<";
+string c1 = ")";
+string c2 = "]";
+string c3 = "}";
+string c4 = ">";
 
+List<string> incomplete = new();
+Dictionary<char, int> illegalCharacters = new();
+foreach (var row in InputHelper.ReadTextFile("input.txt"))
+{
+    bool illegal = false;
+    string chunk = String.Empty;
+    foreach (char c in row)
+    {
+        if (c.IsOpen()) chunk += c;
+        else
+        {
+            if (chunk.Last().IsClosedBy(c)) chunk = chunk.Remove(chunk.Length - 1);
+            else
+            {
+                if (illegalCharacters.ContainsKey(c)) illegalCharacters[c]++;
+                else illegalCharacters.Add(c, 1);
+                illegal = true;
+                break;
+            }
+        }
+    }
+    if(!illegal) incomplete.Add(row);
 }
 
-
-
-
-
-Console.WriteLine($"");
+Console.WriteLine($"Find the first illegal character in each corrupted line of the navigation subsystem. What is the total syntax error score for those errors? {illegalCharacters.ClaculateSyntaxErrorScore()}");
 
 
 Console.WriteLine("\nPart 2");
+List<string> complements = new();
+for (int i = 0; i < incomplete.Count; i++)
+{
+    string complement = String.Empty;
+    bool complete = false;
+    while (!complete)
+    {
+        if (incomplete[i].Last().IsOpen())
+        {
+            complement += incomplete[i].Last().ComplementedBy();
+            incomplete[i] = incomplete[i].Remove(incomplete[i].Length - 1);
+        }
+        else
+        {
+            bool found = false;
+            int countToFind = 1;
+            char c = incomplete[i].Last();
+            incomplete[i] = incomplete[i].Remove(incomplete[i].Length - 1);
+            while (incomplete[i].Length != 0)
+            {
+                
+                if(incomplete[i].Last() == c.ComplementedBy() && countToFind == 1)
+                {
+                    incomplete[i] = incomplete[i].Remove(incomplete[i].Length - 1);
+                    break;
+                }
+                else if (incomplete[i].Last() == c.ComplementedBy() && countToFind != 1)
+                {
+                    countToFind--;
+                    incomplete[i] = incomplete[i].Remove(incomplete[i].Length - 1);
+                }
+                else if(incomplete[i].Last() == c)
+                {
+                    countToFind++;
+                    incomplete[i] = incomplete[i].Remove(incomplete[i].Length - 1);
+                }
+                else incomplete[i] = incomplete[i].Remove(incomplete[i].Length - 1);
+            }
+        }
+
+        if (incomplete[i].Length == 0) break;            
+    }
+    complements.Add(complement);
+}
+
+Console.WriteLine($"Find the completion string for each incomplete line, score the completion strings, and sort the scores. What is the middle score? {Helper.ClaculateCompletionScore(complements)}");
