22e7904f6ff1bba517fe2d227c5a90c48bb4eb1e
Gustav Pettersson <Gustav.Pettersson@knowit.se>
2021-12-11 09:50:53 +0100

    Dag 11

---
+++ Day11/Program.cs
@@ -0,0 +1,63 @@
+﻿
+using Library;
+using Library.Helpers;
+
+Console.WriteLine("--- Day 11: Dumbo Octopus ---");
+
+Console.WriteLine("\nStepping...");
+
+Map map = new Map(InputHelper.ReadComplateTextFile("input.txt").ToList());
+int flashes = 0;
+int allFlashesOn = 0;
+bool part1 = false;
+bool part2 = false;
+for (int i = 0; i < int.MaxValue; i++)
+{
+    map.TickAll();
+    while (true)
+    {
+        List<Position> toFlash = map.GetAll(Enumerable.Range(10, 20).ToArray());
+        if (toFlash.Count == 0) break; 
+        foreach (Position p in toFlash)
+        {
+            map.Set(p, -1);
+            foreach (Position n in p.Neighbors(true))
+            {
+                if (map.Get(n) != null && map.Get(n) == - 1) continue;
+                map.Tick(n);
+            }
+        }
+    }
+    int simultaneouslyFlashed = 0;
+    foreach (Position flashed in map.GetAll(-1))
+    {
+        simultaneouslyFlashed++;
+        flashes++;
+        map.Set(flashed, 0);
+    }
+    if(i == 99)
+    {
+        Console.Write("\r {0} steps done  ", i + 1);
+        part1 = true;
+        Console.WriteLine("\nPart 1");
+        Console.WriteLine($"Given the starting energy levels of the dumbo octopuses in your cavern, simulate 100 steps. How many total flashes are there after 100 steps? {flashes}");
+        if (part1 && part2) break;
+    }
+    if (simultaneouslyFlashed == map.Positions.GetLength(0) * map.Positions.GetLength(1))
+    {
+        Console.Write("\r {0} steps done  ", i + 1);
+        part2 = true;
+        allFlashesOn = i+1;
+        Console.WriteLine("\nPart 2");
+        Console.WriteLine($"If you can calculate the exact moments when the octopuses will all flash simultaneously, you should be able to navigate through the cavern. What is the first step during which all octopuses flash? {allFlashesOn}");
+        if(part1 && part2) break;
+    }
+    Console.Write("\r {0} steps done  ", i+1);
+}
+
+
+
+
+
+
+
---
+++ Library/Map.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Map
+    {
+        public Map(int x, int y)
+        {
+            Positions = new int[x, y];
+        }
+
+        public Map(List<string> input)
+        {
+            Positions = new int[input.Count, input[0].Length];
+            for (int i = 0; i < input.Count; i++)
+            {
+                for (int j = 0; j < input[i].Length; j++)
+                {
+                    Positions[i,j] = (int)Char.GetNumericValue(input[i][j]);
+                }
+            }
+        }
+
+        public int[,] Positions { get; private set; }
+
+        public bool Set(int x, int y, int value)
+        {
+            try
+            {
+                Positions[x, y] = value;
+                return true;
+            }
+            catch (IndexOutOfRangeException)
+            {
+                return false;
+            }
+        }
+        public bool Set(Position p, int value)
+        {
+            return Set(p.X, p.Y, value);
+        }
+
+        public int? Get(Position p)
+        {
+            try
+            {
+                return Positions[p.X, p.Y];
+            }
+            catch (IndexOutOfRangeException)
+            {
+                return null;
+            }
+        }
+
+        public void TickAll(bool down = false)
+        {
+            for (int i = 0; i < Positions.GetLength(0); i++)
+            {
+                for (int j = 0; j < Positions.GetLength(1); j++)
+                {
+                    if(!down)Positions[i, j]++;
+                    else if (down) Positions[i, j]--;
+                }
+            }
+        }
+
+        public List<Position> GetAll(params int[] values)
+        {
+            List<Position> result = new();
+            for (int i = 0; i < Positions.GetLength(0); i++)
+            {
+                for (int j = 0; j < Positions.GetLength(1); j++)
+                {
+                    if(values.Contains(Positions[i, j])) result.Add(new Position(i, j));
+                }
+            }
+            return result;
+        }
+
+        public bool Tick(Position p, bool down = false)
+        {
+            try
+            {
+                if (down) Positions[p.X, p.Y]--;
+                else Positions[p.X, p.Y]++;
+                return true;
+            }
+            catch (IndexOutOfRangeException)
+            {
+                return false;
+            }
+            
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("\n");
+            for (int i = 0; i < Positions.GetLength(0); i++)
+            {
+                string row = "";
+                for (int j = 0; j < Positions.GetLength(1); j++)
+                {
+                    row += Positions[i, j].ToString();
+                }
+                Console.WriteLine(row);
+            }
+        }
+    }
+}
