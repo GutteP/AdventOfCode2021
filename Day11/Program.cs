d281aa65947f777ebe92f6e73e9971297bfafde9
Gustav Pettersson <Gustav.Pettersson@knowit.se>
2021-12-11 09:54:33 +0100

    Cleanup

--- Day11/Program.cs
+++ Day11/Program.cs
@@ -4,13 +4,12 @@ using Library.Helpers;
 
 Console.WriteLine("--- Day 11: Dumbo Octopus ---");
 
-Console.WriteLine("\nStepping...");
-
 Map map = new Map(InputHelper.ReadComplateTextFile("input.txt").ToList());
-int flashes = 0;
-int allFlashesOn = 0;
-bool part1 = false;
-bool part2 = false;
+int totalNumberOfFlashes = 0;
+bool part1done = false;
+bool part2done = false;
+
+Console.WriteLine("\nStepping...");
 for (int i = 0; i < int.MaxValue; i++)
 {
     map.TickAll();
@@ -32,25 +31,28 @@ for (int i = 0; i < int.MaxValue; i++)
     foreach (Position flashed in map.GetAll(-1))
     {
         simultaneouslyFlashed++;
-        flashes++;
+        totalNumberOfFlashes++;
         map.Set(flashed, 0);
     }
+
+    //Part 1
     if(i == 99)
     {
         Console.Write("\r {0} steps done  ", i + 1);
-        part1 = true;
+        part1done = true;
         Console.WriteLine("\nPart 1");
-        Console.WriteLine($"Given the starting energy levels of the dumbo octopuses in your cavern, simulate 100 steps. How many total flashes are there after 100 steps? {flashes}");
-        if (part1 && part2) break;
+        Console.WriteLine($"Given the starting energy levels of the dumbo octopuses in your cavern, simulate 100 steps. How many total flashes are there after 100 steps? {totalNumberOfFlashes}");
+        if (part1done && part2done) break;
     }
+
+    //Part 2
     if (simultaneouslyFlashed == map.Positions.GetLength(0) * map.Positions.GetLength(1))
     {
         Console.Write("\r {0} steps done  ", i + 1);
-        part2 = true;
-        allFlashesOn = i+1;
+        part2done = true;
         Console.WriteLine("\nPart 2");
-        Console.WriteLine($"If you can calculate the exact moments when the octopuses will all flash simultaneously, you should be able to navigate through the cavern. What is the first step during which all octopuses flash? {allFlashesOn}");
-        if(part1 && part2) break;
+        Console.WriteLine($"If you can calculate the exact moments when the octopuses will all flash simultaneously, you should be able to navigate through the cavern. What is the first step during which all octopuses flash? {i + 1}");
+        if(part1done && part2done) break;
     }
     Console.Write("\r {0} steps done  ", i+1);
 }
